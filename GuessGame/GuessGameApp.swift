b91e469eb55c584ee8dd69e32c1cd7a76cad6e2b
kenan551 <kenan.ramazanov1104@gmail.com>
2025-03-28 18:12:44 +0400

    Initial Commit

---
+++ GuessGame/GuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameApp.swift
+//  GuessGame
+//
+//  Created by Kanan  on 28.03.25.
+//
+
+import SwiftUI
+
+@main
+struct GuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuessGameTests/GuessGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameTests.swift
+//  GuessGameTests
+//
+//  Created by Kanan  on 28.03.25.
+//
+
+import Testing
+@testable import GuessGame
+
+struct GuessGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
